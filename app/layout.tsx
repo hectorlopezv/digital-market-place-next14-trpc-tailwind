2b71151f1e640d9432f084fe74e142f94e37ee44
hector lopez <hectorvmlopez@gmail.com>
2024-01-08 16:18:16 -0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <body
           className={cn(
             "min-h-screen font-sans antialiased grainy",
-            fontSans.variable
+            fontSans.className
           )}
         >
           <Toaster />
