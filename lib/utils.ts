d196518a9ccffbc31a89b269eb03d71ab67ec04f
hector lopez <hectorvmlopez@gmail.com>
2024-01-08 16:56:32 -0500

    adding metadata

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +14,49 @@ export function absoluteUrl(path: string) {
     return `https://${process.env.VERCEL_URL}${path}`;
   }
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = "Hector Sass for Pdfs",
+  description = "Generate PDFs from your Sass",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+    },
+    icons,
+    metadataBase: new URL(
+      `https://digital-market-place-next14-trpc-tailwind.vercel.app`
+    ),
+    themeColor: "#ffffff",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
